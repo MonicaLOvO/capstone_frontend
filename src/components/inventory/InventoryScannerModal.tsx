5c949ed16ae09e530f7ff97165f51792216ac7ea
prec12 <chidinmaer1@gmail.com>
2026-03-24 00:33:54 -0600

    added restrictions

--- src/components/inventory/InventoryScannerModal.tsx
+++ src/components/inventory/InventoryScannerModal.tsx
@@ -1,43 +1,43 @@
-"use client";
+// "use client";
 
-import { Modal, ModalDialog, DialogTitle } from "@mui/joy";
-import { Html5QrcodeScanner } from "html5-qrcode";
-import { useEffect } from "react";
+// import { Modal, ModalDialog, DialogTitle } from "@mui/joy";
+// import { Html5QrcodeScanner } from "html5-qrcode";
+// import { useEffect } from "react";
 
-export default function InventoryScannerModal({ open, onClose }: any) {
+// export default function InventoryScannerModal({ open, onClose }: any) {
 
-  useEffect(() => {
+//   useEffect(() => {
 
-    if (!open) return;
+//     if (!open) return;
 
-    const scanner = new Html5QrcodeScanner(
-      "scanner",
-      { fps: 10, qrbox: 250 },
-      false
-    );
+//     const scanner = new Html5QrcodeScanner(
+//       "scanner",
+//       { fps: 10, qrbox: 250 },
+//       false
+//     );
 
-    scanner.render(
-      (decodedText) => {
-        window.location.href = decodedText;
-      },
-      () => {}
-    );
+//     scanner.render(
+//       (decodedText) => {
+//         window.location.href = decodedText;
+//       },
+//       () => {}
+//     );
 
-    return () => {
-      scanner.clear();
-    };
+//     return () => {
+//       scanner.clear();
+//     };
 
-  }, [open]);
+//   }, [open]);
 
-  return (
-    <Modal open={open} onClose={onClose}>
-      <ModalDialog>
+//   return (
+//     <Modal open={open} onClose={onClose}>
+//       <ModalDialog>
 
-        <DialogTitle>Scan QR Code</DialogTitle>
+//         <DialogTitle>Scan QR Code</DialogTitle>
 
-        <div id="scanner" />
+//         <div id="scanner" />
 
-      </ModalDialog>
-    </Modal>
-  );
-}
+//       </ModalDialog>
+//     </Modal>
+//   );
+// }
